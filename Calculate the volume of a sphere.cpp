4d0e632635e2bdeac76ee51fb5ce76bcd68e5aa1
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 12:08:21 +0600

    Add files via upload

---
+++ Calculate the volume of a sphere.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int radius;
+    cout<<"Enter your sphere radius : ";
+    cin>>radius;
+    double vol = (4*3.14*radius*radius*radius)/3;
+    cout<<"The volume of a sphere is :"<< vol <<endl;
+
+
+
+
+    getch();
+}
